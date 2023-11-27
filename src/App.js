0f71b907217fe46c64cc4792f7bed33fbd9d5d37
anvarjon-programmer <mranvarjontojiboyevgmail.com>
2023-11-27 10:23:51 +0300

    lesson-4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+import Card from './pages/Card';
+import Todos from './pages/Todos';
+import Posts from './pages/Posts';
+import Gapley from './pages/Gapley';
+import Koment from './pages/Koment';
+import Undifined from './pages/Undifined';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path='/' element={<Card/>}/>
+          <Route path='/Todos' element={<Todos/>}/>
+          <Route path='/Posts' element={<Posts/>}/>
+          <Route path='/Gapley' element={<Gapley/>}/>
+          <Route path='/Koment' element={<Koment/>}/>
+          <Route path='Undifined' element={<Undifined/>}/>
+        </Routes>
+      </main>
+      <footer></footer>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Loading from './components/loader/loading';
+const App = lazy(() => import ("./App"))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Suspense fallback={<Loading/>}>
+      <App />
+    </Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
